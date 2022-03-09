46455b876b04c3a423bcd8eb27a6500c5e1af0ce
VanekPetr <petrr.vanekk@gmail.com>
2022-03-09 21:45:48 +0100

    patch: updated dictionary with NORD ETFs and created NORD and Lysa portfolios with weights

---
+++ investment_club/nord_and_lysa_portfolios.py
@@ -0,0 +1,102 @@
+# *** PORTFOLIOS ****
+# --------------------------------------------------------------------------------------------------------------------
+# Dictionaries with NORD and Lysa portfolio's composition
+
+nord_5 = {
+    'DB Xtrackers II Eurozone Government Bond UCITS': 40.5,
+    'iShares Core MSCI World UCITS': 18.1,
+    'DB Xtrackers II EUR Corporate Bond UCITS': 16.9,
+    'iShares Core S&P 500 UCITS': 13.0,
+    'iShares Core Euro STOXX 50 UCITS': 4.6,
+    'iShares Core MSCI EM IMI UCITS': 4.5,
+    'iShares Core MSCI Japan IMI UCITS': 2.4
+}
+
+nord_13 = {
+    'DB Xtrackers II Eurozone Government Bond UCITS': 21.6,
+    'iShares Core MSCI World UCITS': 28.8,
+    'DB Xtrackers II EUR Corporate Bond UCITS': 10.5,
+    'iShares Core S&P 500 UCITS': 20.7,
+    'iShares Core Euro STOXX 50 UCITS': 7.4,
+    'iShares Core MSCI EM IMI UCITS': 7.2,
+    'iShares Core MSCI Japan IMI UCITS': 3.8
+}
+
+nord_20 = {
+    'DB Xtrackers II Eurozone Government Bond UCITS': 5.0,
+    'iShares Core MSCI World UCITS': 38.2,
+    'DB Xtrackers II EUR Corporate Bond UCITS': 5.0,
+    'iShares Core S&P 500 UCITS': 27.5,
+    'iShares Core Euro STOXX 50 UCITS': 9.8,
+    'iShares Core MSCI EM IMI UCITS': 9.5,
+    'iShares Core MSCI Japan IMI UCITS': 5.0
+}
+
+lysa_100 = {
+    'Vanguard US': 19.0 * 1.0,
+    'Vanguard North America': 19.0 * 1.0,
+    'Vanguard Europe': 18.0 * 1.0,
+    'Vanguard Emerging Markets': 15.0 * 1.0,
+    'Vanguard Global Small-Cap': 12.0 * 1.0,
+    'iShares US': 6.0 * 1.0,
+    'Vanguard Japan': 6.0 * 1.0,
+    'Vanguard Pacific': 4.0 * 1.0,
+    'Lyxor Europe': 1.0 * 1.0,
+    'Vanguard Europe Government Bond': 15.0 * 0.0,
+    'iShares Europe Government Bond': 14.0 * 0.0,
+    'Vanguard Global Bond': 13.0 * 0.0,
+    'Vanguard Global Short-Term Bond': 12.0 * 0.0,
+    'Vanguard Euro Investment Grade Bond': 12.0 * 0.0,
+    'Vanguard Eurozone Inflation-Linked Bond': 10.85 * 0.0,
+    'Vanguard Global Aggregate Bond': 8.4 * 0.0,
+    'iShares Global Inflation Linked Govt Bond': 5.0 * 0.0,
+    'Vanguard EUR Corporate Bond': 4.4 * 0.0,
+    'iShares Core € Corp Bond': 4.4 * 0.0,
+    'Cash': 0.95 * 0.0
+}
+
+lysa_75_25 = {
+    'Vanguard US': 19.0 * 0.75,
+    'Vanguard North America': 19.0 * 0.75,
+    'Vanguard Europe': 18.0 * 0.75,
+    'Vanguard Emerging Markets': 15.0 * 0.75,
+    'Vanguard Global Small-Cap': 12.0 * 0.75,
+    'iShares US': 6.0 * 0.75,
+    'Vanguard Japan': 6.0 * 0.75,
+    'Vanguard Pacific': 4.0 * 0.75,
+    'Lyxor Europe': 1.0 * 0.75,
+    'Vanguard Europe Government Bond': 15.0 * 0.25,
+    'iShares Europe Government Bond': 14.0 * 0.25,
+    'Vanguard Global Bond': 13.0 * 0.25,
+    'Vanguard Global Short-Term Bond': 12.0 * 0.25,
+    'Vanguard Euro Investment Grade Bond': 12.0 * 0.25,
+    'Vanguard Eurozone Inflation-Linked Bond': 10.85 * 0.25,
+    'Vanguard Global Aggregate Bond': 8.4 * 0.25,
+    'iShares Global Inflation Linked Govt Bond': 5.0 * 0.25,
+    'Vanguard EUR Corporate Bond': 4.4 * 0.25,
+    'iShares Core € Corp Bond': 4.4 * 0.25,
+    'Cash': 0.95 * 0.25
+}
+
+lysa_50_50 = {
+    'Vanguard US': 19.0 * 0.5,
+    'Vanguard North America': 19.0 * 0.5,
+    'Vanguard Europe': 18.0 * 0.5,
+    'Vanguard Emerging Markets': 15.0 * 0.5,
+    'Vanguard Global Small-Cap': 12.0 * 0.5,
+    'iShares US': 6.0 * 0.5,
+    'Vanguard Japan': 6.0 * 0.5,
+    'Vanguard Pacific': 4.0 * 0.5,
+    'Lyxor Europe': 1.0 * 0.5,
+    'Vanguard Europe Government Bond': 15.0 * 0.5,
+    'iShares Europe Government Bond': 14.0 * 0.5,
+    'Vanguard Global Bond': 13.0 * 0.5,
+    'Vanguard Global Short-Term Bond': 12.0 * 0.5,
+    'Vanguard Euro Investment Grade Bond': 12.0 * 0.5,
+    'Vanguard Eurozone Inflation-Linked Bond': 10.85 * 0.5,
+    'Vanguard Global Aggregate Bond': 8.4 * 0.5,
+    'iShares Global Inflation Linked Govt Bond': 5.0 * 0.5,
+    'Vanguard EUR Corporate Bond': 4.4 * 0.5,
+    'iShares Core € Corp Bond': 4.4 * 0.5,
+    'Cash': 0.95 * 0.5
+}
