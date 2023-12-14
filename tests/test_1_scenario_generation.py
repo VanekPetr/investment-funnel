d51b94681ddf3304d2a623c5fa8e8c9e660e87e1
VanekPetr <petrr.vanekk@gmail.com>
2023-12-14 20:14:54 +0100

    fix: tests for scenario generation

--- tests/test_1_scenario_generation.py
+++ tests/test_1_scenario_generation.py
@@ -8,7 +8,7 @@ TEST_DIR = Path(__file__).parent
 def test_monte_carlo_scenarios(mc_scenarios):
     expected_scenarios = np.load("tests/scgen/scenarios_BASE.npz")["scenarios"]
 
-    # np.savez_compressed("tests/scgen/scenarios_ACTUAL.npz", scenarios=scenarios)
+    # np.savez_compressed("scgen/scenarios_BASE.npz", scenarios=scenarios)
     np.testing.assert_array_equal(mc_scenarios, expected_scenarios)
 
 
@@ -19,5 +19,13 @@ def test_moments(moments):
     expected_mu_list = list(expected_mus[k] for k in expected_mus)
 
     sigma_list, mu_list = moments
+    # np.savez_compressed(
+    #     "scgen/mu_list_BASE.npz",
+    #     **dict(zip([f"mu_{i}" for i in range(len(mu_list))], mu_list))
+    # )
+    # np.savez_compressed(
+    #     "scgen/sigma_list_BASE.npz",
+    #     **dict(zip([f"sigma_{i}" for i in range(len(sigma_list))], sigma_list))
+    # )
     np.testing.assert_array_equal(mu_list, expected_mu_list)
     np.testing.assert_array_equal(sigma_list, expected_sigma_list)
