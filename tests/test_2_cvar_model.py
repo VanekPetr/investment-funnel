54525af211c47933ff2cae45c457ca1c51841714
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-12-15 06:21:13 +0400

    cvar_folder

--- tests/test_2_cvar_model.py
+++ tests/test_2_cvar_model.py
@@ -29,18 +29,21 @@ def cvar_target_data(
     )
     return targets, benchmark_port_val
 
+@pytest.fixture(scope="module")
+def cvar_dir(resource_dir):
+    return resource_dir / "cvar"
+
 
 @pytest.mark.parametrize(
     "cvar_target_data, label",
     [("benchmark_isin_1", "1"), ("benchmark_isin_2", "2")],
     indirect=["cvar_target_data"],
 )
-def test_get_cvar_targets(cvar_target_data, label, resource_dir):
-    expected_targets = pd.read_csv(
-        resource_dir / f"cvar/targets_{label}_BASE.csv", index_col=0
-    )
+
+def test_get_cvar_targets(cvar_target_data, label, cvar_dir):
+    expected_targets = pd.read_csv( cvar_dir / f"targets_{label}_BASE.csv", index_col=0)
     expected_benchmark_port_val = pd.read_csv(
-        resource_dir / f"cvar/benchmark_port_val_{label}_BASE.csv",
+        cvar_dir / f"benchmark_port_val_{label}_BASE.csv",
         index_col=0,
         parse_dates=True,
     )
@@ -50,23 +53,21 @@ def test_get_cvar_targets(cvar_target_data, label, resource_dir):
 
     targets, benchmark_port_val = cvar_target_data
 
-    # targets.to_csv(f"cvar/targets_{label}_BASE.csv")
-    # benchmark_port_val.to_csv(f"cvar/benchmark_port_val_{label}_BASE.csv")
+    # targets.to_csv(cvar_dir / f"targets_{label}_BASE.csv")
+    # benchmark_port_val.to_csv(cvar_dir / f"benchmark_port_val_{label}_BASE.csv")
     pd.testing.assert_frame_equal(targets, expected_targets)
     pd.testing.assert_frame_equal(benchmark_port_val, expected_benchmark_port_val)
 
 
 @pytest.mark.parametrize("cvar_target_data", ["benchmark_isin_2"], indirect=True)
-def test_cvar_model(test_narrow_dataset, mc_scenarios, cvar_target_data, resource_dir):
+def test_cvar_model(test_narrow_dataset, mc_scenarios, cvar_target_data, resource_dir, cvar_dir):
     expected_port_allocation = pd.read_csv(
-        resource_dir / "cvar/port_allocation_BASE.csv", index_col=0
+        cvar_dir / "port_allocation_BASE.csv", index_col=0
     )
     expected_port_value = pd.read_csv(
-        resource_dir / "cvar/port_value_BASE.csv", index_col=0, parse_dates=True
+        cvar_dir / "port_value_BASE.csv", index_col=0, parse_dates=True
     )
-    expected_port_cvar = pd.read_csv(
-        resource_dir / "cvar/port_cvar_BASE.csv", index_col=0
-    )
+    expected_port_cvar = pd.read_csv(cvar_dir / "port_cvar_BASE.csv", index_col=0)
 
     # np.savez_compressed("scgen/scenarios_BASE.npz", scenarios=mc_scenarios)
     generated_scenarios = np.load(resource_dir / "scgen/scenarios_BASE.npz")[
@@ -87,9 +88,9 @@ def test_cvar_model(test_narrow_dataset, mc_scenarios, cvar_target_data, resourc
         lower_bound=0,
     )
 
-    # port_allocation.to_csv("cvar/port_allocation_BASE.csv")
-    # port_value.to_csv("cvar/port_value_BASE.csv")
-    # port_cvar.to_csv("cvar/port_cvar_BASE.csv")
+    # port_allocation.to_csv(cvar_dir / "port_allocation_BASE.csv")
+    # port_value.to_csv(cvar_dir / "port_value_BASE.csv")
+    # port_cvar.to_csv(cvar_dir / "port_cvar_BASE.csv")
 
     active_constraints = (targets.to_numpy() - port_cvar.to_numpy()) < 1e-5
     pd.testing.assert_frame_equal(port_allocation, expected_port_allocation, atol=1e-5)
