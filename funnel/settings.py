518d2fd4e43c7b3c3b8df05f338da22508de419c
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-25 20:51:33 +0400

    settings not needed for gui

--- funnel/settings.py
+++ funnel/settings.py
@@ -1,16 +1,16 @@
-from dotenv import load_dotenv
-from pydantic_settings import BaseSettings
-
-load_dotenv()
-
-
-class Settings(BaseSettings):
-    # Would you like to download the latest AlgoStrata's data?
-    # TODO write to kourosh@algostrata.dk to get your own API key
-    NAME: str = "Investment Funnel Secrets"
-    ALGOSTRATA_NAMES_URL: str = "private"
-    ALGOSTRATA_PRICES_URL: str = "notgoingtotellyou"
-    ALGOSTRATA_KEY: str = "nonono"
-
-
-settings = Settings()
+# from dotenv import load_dotenv
+# from pydantic_settings import BaseSettings
+#
+# load_dotenv()
+#
+#
+# class Settings(BaseSettings):
+#     # Would you like to download the latest AlgoStrata's data?
+#     # TODO write to kourosh@algostrata.dk to get your own API key
+#     NAME: str = "Investment Funnel Secrets"
+#     ALGOSTRATA_NAMES_URL: str = "private"
+#     ALGOSTRATA_PRICES_URL: str = "notgoingtotellyou"
+#     ALGOSTRATA_KEY: str = "nonono"
+#
+#
+# settings = Settings()
