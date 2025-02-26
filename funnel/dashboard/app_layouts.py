d0edd5fcf17cc1943c0d0e1bd7596a54ef799265
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-26 10:35:41 +0400

    moving market_overview

--- funnel/dashboard/app_layouts.py
+++ funnel/dashboard/app_layouts.py
@@ -1,5 +1,6 @@
 import dash_bootstrap_components as dbc
 from dash import html
+from ifunnel.models.main import initialize_bot
 
 from .components_and_styles.ai_feature_selection_page import (
     graphML,
@@ -17,11 +18,17 @@ from .components_and_styles.lifecycle_page import (
     results_lifecycle,
     spinner_lifecycle,
 )
-from .components_and_styles.market_overview_page import (
-    graphOverview,
-    optionGraph,
-    spinner_dots,
-)
+from .components_and_styles.market_overview_page import divs as market_overview
+
+algo = initialize_bot()
+
+overview = market_overview(algo)
+
+#
+#    (graphOverview,
+#    optionGraph,
+#    spinner_dots),
+#)
 
 # *** LIFECYCLE ***
 page_4_layout = html.Div(
@@ -84,9 +91,9 @@ page_1_layout = html.Div(
                 # Row 1, Col 1 - navigation bar
                 dbc.Col([sideBar]),
                 # Row 1, col 2 - text description
-                dbc.Col([optionGraph]),
+                dbc.Col([overview.optionsGraph]),
                 # Row 1, Col 3 - table
-                dbc.Col([graphOverview, spinner_dots]),
+                dbc.Col([overview.graphOverview, overview.spinner_dots]),
             ]
         )
     ]
