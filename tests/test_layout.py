906ea124e902b55bd8a8aad4f22a04c4bb1d4703
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-27 15:21:56 +0400

    better testing

--- tests/test_layout.py
+++ tests/test_layout.py
@@ -1,4 +1,5 @@
 from funnel.dashboard.app_layouts import divs as app_layouts
+from funnel.dashboard.app_layouts import load_page
 from funnel.dashboard.components_and_styles.ai_feature_selection_page import divs as ai_feature_selection
 from funnel.dashboard.components_and_styles.backtest_page import divs as backtest
 from funnel.dashboard.components_and_styles.lifecycle_page import divs as lifecycle
@@ -24,3 +25,7 @@ def test_ai_feature_selection(algo):
 def test_layouts(algo):
     x = app_layouts(algo)
     assert x is not None
+
+def test_load_page(algo):
+    x = app_layouts(algo)
+    load_page(x.page_1, algo)
---
+++ tests/test_logging.py
@@ -0,0 +1,6 @@
+# from funnel.dashboard.utils.logger import setup_logging
+#
+#
+# def test_logging():
+#     logger = setup_logging()
+#     logger.info("Hello World")
